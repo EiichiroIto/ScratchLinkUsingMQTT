dbdfea42c6789f3834f45ba85795815ee38ba35f
Eiichiro Ito <eiichiro.ito@gmail.com>
2025-11-19 21:22:19 +0900

    Fix some bugs

--- python/stack-chan.py
+++ python/stack-chan.py
@@ -3,8 +3,9 @@ import json
 import base64
 import time
 from threading import Timer
-import serial
+from serial import Serial, SerialException
 
+without_sensor = False
 connecting = False
 uart = None
 
@@ -15,27 +16,35 @@ def update(client,server):
     server.send_message(client, response)
 
     while connecting:
-        if uart:
-            r = uart.read_until(b'\n').strip()
-            print(r)
+        if uart and not without_sensor:
+            try:
+                r = uart.read_until(b'\n').strip()
+            except Exception as e:
+                print(e)
+                break
+            #print(r)
             if len(r)>0 and r[0]==42:
-                s = list(map(int,r[1:].decode('utf-8').split(',')))
-                print(s)
-                tiltX = 0
-                tiltY = 0
-                if len(s) and s[0] == 1:
-                    tiltX = int(s[1]) * 10
-                    tiltY = int(s[2]) * 10
-                buf = bytearray(10)
-                buf[0] = tiltX // 256
-                buf[1] = tiltX % 256
-                buf[2] = tiltY // 256
-                buf[3] = tiltY % 256
-                buf[4] = 1 if int(s[3]) > 50 else 0
-                b64 = base64.b64encode(bytes(buf)).decode('utf-8')
-                response = '{"jsonrpc":"2.0","method":"characteristicDidChange","params":{"serviceId":61445,"characteristicId":"5261da01-fa7e-42ab-850b-7c80220097cc","encoding":"base64","message":"' + b64 + '"}}'
-                server.send_message(client, response)
-                print(response)
+                try:
+                    s = list(map(int,r[1:].decode('utf-8').split(',')))
+                except Exception as e:
+                    print(e)
+                else:
+                    tiltX = 0
+                    tiltY = 0
+                    if len(s) and s[0] == 1:
+                        print(s)
+                        tiltX = int(s[1]) * 10
+                        tiltY = int(s[2]) * 10
+                    buf = bytearray(10)
+                    buf[0] = tiltX // 256
+                    buf[1] = tiltX % 256
+                    buf[2] = tiltY // 256
+                    buf[3] = tiltY % 256
+                    buf[4] = 1 if int(s[3]) > 50 else 0
+                    b64 = base64.b64encode(bytes(buf)).decode('utf-8')
+                    response = '{"jsonrpc":"2.0","method":"characteristicDidChange","params":{"serviceId":61445,"characteristicId":"5261da01-fa7e-42ab-850b-7c80220097cc","encoding":"base64","message":"' + b64 + '"}}'
+                    server.send_message(client, response)
+                    #print(response)
         else:
             b64 = base64.b64encode(bytes(10)).decode('utf-8')
             response = '{"jsonrpc":"2.0","method":"characteristicDidChange","params":{"serviceId":61445,"characteristicId":"5261da01-fa7e-42ab-850b-7c80220097cc","encoding":"base64","message":"' + b64 + '"}}'
@@ -67,7 +76,7 @@ def message_received(client, server, message):
     if dict['method'] == 'connect':
         id = dict['id']
         try:
-            uart = serial.Serial("/dev/ttyUSB0", 115200)
+            uart = Serial("/dev/ttyUSB0", 115200)
         except SerialException:
             print("can't open /dev/ttyUSB0")
             uart = None
