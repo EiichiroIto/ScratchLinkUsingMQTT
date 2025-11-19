2c05bc68b3827f3888a3b0a85cf2e51fd75bb094
Eiichiro Ito <eiichiro.ito@gmail.com>
2025-11-19 21:23:46 +0900

    Add classes

--- python/stack-chan.py
+++ python/stack-chan.py
@@ -6,50 +6,106 @@ from threading import Timer
 from serial import Serial, SerialException
 
 without_sensor = False
-connecting = False
-uart = None
 
-def update(client,server):
-    time.sleep(3)
-    b64 = base64.b64encode(bytes([0,123,1,0,1,1,1,1,1,1])).decode('utf-8')
-    response = '{"jsonrpc":"2.0","method":"characteristicDidChange","params":{"serviceId":61445,"characteristicId":"5261da01-fa7e-42ab-850b-7c80220097cc","encoding":"base64","message":"' + b64 + '"}}'
-    server.send_message(client, response)
+class StackChan:
+    def __init__(self):
+        self.uart = None
+    def is_connected(self):
+        return self.uart != None
+    def open(self):
+        self.uart = None
+        try:
+            self.uart = Serial("/dev/ttyUSB0", 115200)
+        except SerialException:
+            print("can't open /dev/ttyUSB0")
+        return self.is_connected()
+    def _send(self, text):
+        print(text)
+        self.uart.write(text.encode('utf-8'))
+    def clear(self):
+        if not self.uart:
+            return
+        self._send('*0')
+    def servo(self, ch, degrees):
+        if not self.uart:
+            return
+        if not (ch == 1 or ch == 2):
+            return
+        self._send('*%d,%d' % (ch, degrees))
+    def say(self, message):
+        if not self.uart:
+            return
+        self._send('*3,'+','.join(list(map(str,message))))
+    def expression(self, kind):
+        if not self.uart:
+            return
+        self._send('*4,%d' % kind)
+    def receive(self):
+        if not self.uart:
+            return None
+        try:
+            r = self.uart.read_until(b'\n').strip()
+        except Exception as e:
+            print(e)
+            return None
+        if not (len(r)>0 and r[0] == 42):
+            return None
+        try:
+            s = list(map(int,r[1:].decode('utf-8').split(',')))
+        except Exception as e:
+            print(e)
+            return None
+        tiltX = 0
+        tiltY = 0
+        if len(s) and s[0] == 1:
+            print(s)
+            tiltX = int(s[1]) * 10
+            tiltY = int(s[2]) * 10
+        buf = bytearray(10)
+        buf[0] = tiltX // 256
+        buf[1] = tiltX % 256
+        buf[2] = tiltY // 256
+        buf[3] = tiltY % 256
+        buf[4] = 1 if int(s[3]) > 50 else 0
+        return buf
 
-    while connecting:
-        if uart and not without_sensor:
-            try:
-                r = uart.read_until(b'\n').strip()
-            except Exception as e:
-                print(e)
-                break
-            #print(r)
-            if len(r)>0 and r[0]==42:
-                try:
-                    s = list(map(int,r[1:].decode('utf-8').split(',')))
-                except Exception as e:
-                    print(e)
-                else:
-                    tiltX = 0
-                    tiltY = 0
-                    if len(s) and s[0] == 1:
-                        print(s)
-                        tiltX = int(s[1]) * 10
-                        tiltY = int(s[2]) * 10
-                    buf = bytearray(10)
-                    buf[0] = tiltX // 256
-                    buf[1] = tiltX % 256
-                    buf[2] = tiltY // 256
-                    buf[3] = tiltY % 256
-                    buf[4] = 1 if int(s[3]) > 50 else 0
-                    b64 = base64.b64encode(bytes(buf)).decode('utf-8')
-                    response = '{"jsonrpc":"2.0","method":"characteristicDidChange","params":{"serviceId":61445,"characteristicId":"5261da01-fa7e-42ab-850b-7c80220097cc","encoding":"base64","message":"' + b64 + '"}}'
-                    server.send_message(client, response)
-                    #print(response)
+class ScratchLink:
+    def __init__(self):
+        self.close()
+    def open(self):
+        self.connected = True
+    def close(self):
+        self.connected = False
+    def is_connected(self):
+        return self.connected
+    def characteristicDidChange(self, server, client, buf):
+        response = '{"jsonrpc":"2.0","method":"characteristicDidChange","params":{"serviceId":61445,"characteristicId":"5261da01-fa7e-42ab-850b-7c80220097cc","encoding":"base64","message":"%s"}}' % (base64.b64encode(bytes(buf)).decode('utf-8'),)
+        server.send_message(client, response)
+    def result(self, server, client, id, ok):
+        if ok:
+            response = '{"jsonrpc":"2.0","id":%d,"result":null}' % id
         else:
-            b64 = base64.b64encode(bytes(10)).decode('utf-8')
-            response = '{"jsonrpc":"2.0","method":"characteristicDidChange","params":{"serviceId":61445,"characteristicId":"5261da01-fa7e-42ab-850b-7c80220097cc","encoding":"base64","message":"' + b64 + '"}}'
-            server.send_message(client, response)
-            print(response)
+            response = '{"jsonrpc":"2.0","id":%d,"error":{}}' % id
+        server.send_message(client, response)
+    def didDiscoverPeripheral(self, server, client, name, rssi, peripheralId):
+        response = '{"jsonrpc":"2.0","method":"didDiscoverPeripheral","params":{"name":"%s","rssi":%d,"peripheralId":%d}}' % (name, rssi, peripheralId)
+        server.send_message(client, response)
+
+stackchan = StackChan()
+scratchlink = ScratchLink()
+
+def update(client, server):
+    time.sleep(2)
+    scratchlink.characteristicDidChange(server, client, bytes(10))
+    time.sleep(2)
+    scratchlink.characteristicDidChange(server, client, bytes(10))
+    while scratchlink.is_connected():
+        if stackchan.is_connected() and not without_sensor:
+            buf = stackchan.receive()
+            if buf:
+                scratchlink.characteristicDidChange(server, client, buf)
+        else:
+            scratchlink.characteristicDidChange(server, client, bytes(10))
             time.sleep(1)
     print("Update exited")
 
@@ -57,60 +113,42 @@ def new_client(client, server):
     print("New client connected: %d" % client['id'])
 
 def client_left(client, server):
-    global connecting
     print("Client disconnected: %d" % client['id'])
-    connecting = False
+    scratchlink.close()
 
 def message_received(client, server, message):
-    global connecting, uart
     if len(message) > 200:
         message = message[:200]+'..'
     print("Client(%d) said: %s" % (client['id'], message))
     dict = json.loads(message)
     if dict['method'] == 'discover':
         id = dict['id']
-        response = '{"jsonrpc":"2.0","id":'+str(id)+',"result":null}'
-        server.send_message(client, response)
-        response = '{"jsonrpc":"2.0","method":"didDiscoverPeripheral","params":{"name":"python","rssi":-70,"peripheralId":65536}}'
-        server.send_message(client, response)
+        scratchlink.result(server, client, id, True)
+        scratchlink.didDiscoverPeripheral(server, client, "python", -70, 65536)
     if dict['method'] == 'connect':
         id = dict['id']
-        try:
-            uart = Serial("/dev/ttyUSB0", 115200)
-        except SerialException:
-            print("can't open /dev/ttyUSB0")
-            uart = None
-            response = '{"jsonrpc":"2.0","id":'+str(id)+',"error":{}}'
-        else:
-            response = '{"jsonrpc":"2.0","id":'+str(id)+',"result":null}'
-        server.send_message(client, response)
+        ret = stackchan.open()
+        scratchlink.result(server, client, id, ret)
+        if ret:
+            scratchlink.open()
     if dict['method'] == 'read':
-        connecting = True
         timer = Timer(1, update, (client, server))
         timer.start()
     if dict['method'] == 'write':
         message = base64.b64decode(dict['params']['message'])
         if message[0] == 0x81:
-            print(message[1:])
-            print('Say "'+message[1:].decode('utf-8')+'"')
-            if uart:
-                v = '*3,'+','.join(list(map(str,message[1:])))
-                print(v)
-                uart.write(v.encode('utf-8'))
+            stackchan.say(message[1:])
         elif message[0] == 0x82:
             for x in list(message[1:]):
                 print(format(x,"05b")[::-1])
-            if uart:
-                v = '*0'
-                if message[1] == 1 or message[1] == 2:
-                    v = '*'+str(message[1])+','+str(message[2]*32 + message[3])
-                elif message[1] == 4:
-                    v = '*4,'+str(message[2])
-                print(v)
-                uart.write(v.encode('utf-8'))
+            if message[1] == 1 or message[1] == 2:
+                stackchan.servo(message[1], message[2]*32 + message[3])
+            elif message[1] == 4:
+                stackchan.expression(message[2])
+            else:
+                stackchan.clear()
         id = dict['id']
-        response = '{"jsonrpc":"2.0","id":'+str(id)+',"result":null}'
-        server.send_message(client, response)
+        scratchlink.result(server, client, id, True)
 
 PORT=20111
 server = WebsocketServer(port = PORT)
